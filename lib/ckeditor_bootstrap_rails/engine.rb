8dc22a66889b40db97d29f164baa77d14e42fa7d
Yaroslav Konoplov <the.yivo@gmail.com>
2015-03-05 19:22:05 +0200

    Added images to precompile

--- lib/ckeditor_bootstrap_rails/engine.rb
+++ lib/ckeditor_bootstrap_rails/engine.rb
@@ -2,6 +2,28 @@ module CkeditorBootstrap
   module Rails
     class Engine < ::Rails::Engine
 
+      initializer :images do |app|
+        images = %w(
+          arrow.png
+
+          close.png
+          lock.png
+          lock-open.png
+          refresh.png
+
+          hidpi/close.png
+          hidpi/lock.png
+          hidpi/lock-open.png
+          hidpi/refresh.png
+
+          icons.png
+          icons_hidpi.png
+        )
+
+        images.map! { |path| "ckeditor_bootstrap/#{path}" }
+        app.config.assets.precompile += images
+      end
+
       initializer :stylesheets do |app|
 
         # Add scripts to precompile
